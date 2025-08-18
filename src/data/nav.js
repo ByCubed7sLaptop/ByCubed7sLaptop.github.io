6e23b5b1bbec5060fbfe3dd0594206fa3237cd5f
Ethan <ByCubed7+github@gmail.com>
2025-08-18 06:03:08 +0100

    Redirects to domain correctly

--- src/data/nav.js
+++ src/data/nav.js
@@ -2,4 +2,4 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 export const Home = isDevelopment
     ? "http://localhost:5173/"
-    : "https://bycubed7slaptop.github.io/";
+    : "https://thatoneprogrammer.uk/";
