7959bdc228d4c75b969604fb9921b8741e88c3ee
ByCubed7sLaptop <github@ThatOneProgrammer.uk>
2026-04-30 06:23:49 +0100

    Added some interpolation

--- src/components/horizontalScroll.jsx
+++ src/components/horizontalScroll.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './horizontalScroll.module.css';
 
+function lerp(a, b, t) {
+	return a + (b - a) * t;
+}
+
 export default function HorizontalScroll({ style, children, speed }) {
     const scrollRef = useRef(null);
 
@@ -13,16 +17,24 @@ export default function HorizontalScroll({ style, children, speed }) {
         let nudge = 0;
         let mult = 1;
 
+        let min = 0;
+        let max = el.clientWidth / 2;
+        let margin = 0.1;
+        let current = 0;
+
         const scroll = () => {
             if (!el) return;
 
             nudge += speed * mult
 
-            el.scrollLeft = nudge;
-            if (nudge >= el.clientWidth / 2 || nudge < 0) {
-                nudge = Math.min(Math.max(nudge, 0), el.clientWidth / 2 - 1)
+            if (nudge > max - margin || nudge < min + margin) {
+                nudge = Math.min(Math.max(nudge, min), max)
                 mult *= -1;
             }
+
+            current = lerp(current, nudge, 0.05)
+
+            el.scrollLeft = current;
 
             animationFrame = requestAnimationFrame(scroll);
         };
